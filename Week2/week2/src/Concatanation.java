f39d504448cf9cb880b4694aef54aae433ef6a29
Andrea E <117130590+andreamartinez13@users.noreply.github.com>
2023-04-21 14:34:33 -0700

    Add files via upload

---
+++ Week2/week2/src/Concatanation.java
@@ -0,0 +1,14 @@
+public class Concatanation {
+    public static void main(String[] args) {
+
+        String color = "red";
+        System.out.println("My favorite color is " +color);
+
+        int money = 100;
+        System.out.println("I won $" +money+ " dollars!");
+
+        System.out.println(color+"-"+money);
+
+    }
+
+}
