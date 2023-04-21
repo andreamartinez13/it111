628c25c434404ec50a9840ef2d597563aae8947b
Andrea E <117130590+andreamartinez13@users.noreply.github.com>
2023-04-21 14:34:03 -0700

    Add files via upload

---
+++ Week1/src/Week2/week2/src/MyVariable1.java
@@ -0,0 +1,29 @@
+public class MyVariable1 {
+
+    public static void main(String[] args) {
+        String name = "Andrea"; // After a statement do not forget semicolon ;
+        int hours = 40; // we are assigning 40 (number to hours variable. No quotes on int variables (or numbers)
+        System.out.println(name);
+        System.out.println(hours);
+
+        String country, city, street;
+        country = "Europe";
+        city = "Mexico";
+        street = "Boren Av";
+
+        System.out.println(country);
+        System.out.println(city);
+        System.out.println(street);
+
+        int weeks, days, month;
+        weeks = 4;
+        days = 365;
+        month = 12;
+
+
+
+
+
+    }
+
+}
