0ec070f647c7e81299705a40a7c330a024128d3a
Andrea E <117130590+andreamartinez13@users.noreply.github.com>
2023-05-20 00:08:29 -0700

    Update ShowWindow3.java

--- week6/src/ShowWindow3.java
+++ week6/src/ShowWindow3.java
@@ -7,8 +7,8 @@ public class ShowWindow3 extends JFrame {
     JTextField whitespace;
     JTextField whitespace2;
 
-    final int WINDOW_WIDTH = 900;
-    final int WINDOW_HEIGHT = 900;
+    final int WINDOW_WIDTH = 500;
+    final int WINDOW_HEIGHT = 700;
 
     public static void main(String[] args) {
         new ShowWindow3();
@@ -19,27 +19,24 @@ public class ShowWindow3 extends JFrame {
         setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
         getContentPane().setBackground(Color.red);
         setLocationRelativeTo(null);
+        setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         messageLabel = new JLabel("My First Big Label");
+        messageLabel.setFont(new Font("Verdana", Font.PLAIN, 18));
         messageLabel2 = new JLabel("My First Big Label 2");
-//        messageLabel.setForeground(Color.black);
-//        messageLabel.setVerticalTextPosition(JLabel.BOTTOM);
-//        messageLabel.setHorizontalTextPosition(JLabel.CENTER);
-//        whitespace.add(messageLabel);
+        messageLabel2.setFont(new Font("Verdana", Font.BOLD,24));
+        whitespace = new JTextField();
+        whitespace2 = new JTextField();
+
+        messageLabel.setBounds(0,0,500,50);
+        whitespace.setBounds(0,50,450,80);
+        messageLabel2.setBounds(0,150,500,50);
+        whitespace2.setBounds(0,200, 450,80);
+
         add(messageLabel);
         add(messageLabel2);
-
-        whitespace = new JTextField(10);
-        whitespace2 = new JTextField(10);
-//        whitespace.setSize(new Dimension(10,10));
-//        whitespace.setBounds(0,0,100,100);
-        whitespace.setBackground(Color.BLACK);
-//        whitespace.setLocation(0,0);
-        whitespace2.setBounds(0,0, 100,100);
-        whitespace2.setBackground(Color.BLUE);
         add(whitespace);
-
         add(whitespace2);
 
         setVisible(true);
